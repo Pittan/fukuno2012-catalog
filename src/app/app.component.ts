60770ecc781684ad2016caf5be9dbe874c2d0c47
Amon Keishima <amon.keishima@gmail.com>
2017-06-03 15:12:23 +0900

    WIP: implementing app list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.styl']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+
+  apps: any[] = [];
+
+  constructor(private http: Http) { }
+
+  ngOnInit() {
+    this.http.get('/assets/catalog.json')
+      .subscribe(res => {
+        const data = res.json();
+        this.apps = data.item;
+        console.log(this.apps);
+      }
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AppCellComponent } from './app-cell/app-cell.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppCellComponent
   ],
   imports: [
     BrowserModule,

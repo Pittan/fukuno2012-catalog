bbffd9fcc7f76d31e3696bd8429a14c9bbbbf31f
Amon Keishima <amon.keishima@gmail.com>
2017-06-13 23:18:34 +0900

    👕 lint fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'fc-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.styl']
 })
